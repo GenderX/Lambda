844d4fa75a76839b7f9e9a68983ef8846087bee3
lican <lic@56join.com>
2019-01-04 13:41:55 +0800

    添加冒泡排序

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -3,3 +3,5 @@
 src/Person.java,0\2\029dfd4797368bfce5c2cb772c60b54a595ad1c6
 =
 src/Main.java,9\2\924f089edb4af5ee45c8a9d1129cd28e2b368138
+=
+src/sort.java,0\e\0e8d4e445ef28455230e892d3d89c6aa371a1263
--- src/sort.java
+++ src/sort.java
@@ -1,39 +1,64 @@
 public class sort {
 
-    public void sort(int arr[] ,int low, int high){
-        int l=low;
-        int h=high;
-        int povit=arr[low];
-        while (l<h){
-            while (l<h&&arr[h]>=povit)
+    /**
+     * 快速排序
+     *
+     * @param arr
+     * @param low
+     * @param high
+     */
+    public void sort(int arr[], int low, int high) {
+        int l = low;
+        int h = high;
+        int povit = arr[low];
+        while (l < h) {
+            while (l < h && arr[h] >= povit)
                 h--;
-            if(l<h){
-                int temp=arr[h];  //注释测试
-                arr[h]=arr[l];
-                arr[l]=temp;
+            if (l < h) {
+                int temp = arr[h];
+                arr[h] = arr[l];
+                arr[l] = temp;
                 l++;
             }
-            while (l<h&&arr[l]<=povit)
+            while (l < h && arr[l] <= povit)
                 l++;
-            if (l<h){
-                int temp=arr[h];
-                arr[h]=arr[l];
-                arr[l]=temp;
+            if (l < h) {
+                int temp = arr[h];
+                arr[h] = arr[l];
+                arr[l] = temp;
                 h--;
             }
         }
-        if(l>low) sort(arr,low,l-1);
-        if (h<high) sort(arr,l+1,high);
+        if (l > low) sort(arr, low, l - 1);
+        if (h < high) sort(arr, l + 1, high);
+    }
+
+
+    /**
+     * 冒泡排序
+     *
+     * @param array
+     */
+    public void bubbleSort(int[] array) {
+        for (int i = 0; i < array.length - 1; i++) {
+            for (int j = 0; j < array.length - i - 1; j++) {//-1为了防止溢出
+                if (array[j] > array[j + 1]) {
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
     }
 
 
     public static void main(String[] args) {
-        int[] a={10,8,5,9,100,5,1,0,3,99};
-
+        int[] a = {10, 8, 5, 9, 100, 5, 1, 0, 3, 99};
         sort sort = new sort();
-        sort.sort(a,0,a.length-1);
+        //   sort.sort(a,0,a.length-1);
+        sort.bubbleSort(a);
         for (int i = 0; i < a.length; i++) {
-            System.out.print(a[i]+ " ");
+            System.out.print(a[i] + " ");
         }
 
     }
